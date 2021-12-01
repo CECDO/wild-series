a49e5f37db5e7aaf015920c608ac16228aec62d1
CECILE <cecdonas@gmail.com>
2021-12-01 11:25:26 +0100

    symfony5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
 Class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
     return $this->render('program/index.html.twig', [
        'website' => 'Wild Séries',
+    ]);
+    }
+     /**
+     * @Route("/{id}", methods={"GET"}, name="program_SHOW", requirements={"id"="\d+"})
+     */
+    public function show(int $id): Response
+    {
+    return $this->render('program/show.html.twig', [
+       'id' => $id,
     ]);
     }
 }
