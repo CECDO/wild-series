0bea9b6e65b497a46a09885eb00aaece37499747
CECILE <cecdonas@gmail.com>
2021-12-01 20:03:36 +0100

    symfony 7

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -32,6 +32,12 @@ class Program
      */
     private $poster;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +75,18 @@ class Program
     public function setPoster(?string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
