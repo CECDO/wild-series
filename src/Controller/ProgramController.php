6b0cc0be7bc248eb1b985b5d7e3d5a3339541215
CECILE <cecdonas@gmail.com>
2021-12-02 15:57:49 +0100

    correctif

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\program;
+use App\Entity\Program;
 
 /**
 * @Route("/program", name="program_")
