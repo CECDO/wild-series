06459cb73c6e50e1f5976e402f97c6f315a9a6b9
CECILE <cecdonas@gmail.com>
2022-01-03 23:45:47 +0100

    symfony20 v1

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -10,6 +10,14 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+    const PROGRAMS = [
+        'Walking Dead',
+        'The Witcher',
+        'Peaky Blinders',
+        'Sons of anarchy',
+        'The It Crowd',
+    ];
+
     private Slugify $slugify;
 
     public function __construct(Slugify $slugify)
@@ -20,19 +28,23 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
     {
         // $product = new Product();
         // $manager->persist($product);
+        foreach (self::PROGRAMS as $key => $programName) {
+            $program = new Program();
             $program->setTitle($programName);
             $program->setSlug($this->slugify->generate($programName));
             $program->setSynopsis('Une super série');
             $program->setYear('2010');
             $program->setPoster('https://fr.web.img6.acsta.net/c_210_280/pictures/210/454/21045474_20130930201634487.jpg');
             $program->setCountry('USA');
-            $program->setOwner($this->getReference('contributor_0'));
             $program->setCategory($this->getReference('category_0'));
             for ($i=0; $i < count(ActorFixtures::ACTORS); $i++) {
                 $program->addActor($this->getReference('actor_' . $i));
         }
+        $this->addReference('program_' . $key, $program);
         $manager->persist($program);
+    }
         $manager->flush();
+    
     }
 
          public function getDependencies()
